c94838bda5f1efcf78b3ed0edd776e2d5f802c73
andneo <a.neo@windowslive.com>
2023-06-29 12:52:36 +0100

    update

--- static/js/snowflakes.js
+++ static/js/snowflakes.js
@@ -14,13 +14,14 @@ let noise;
 let canvas;
 
 // function windowResized() {
-//   //console.log('resized');
-//   canvas.resizeCanvas(windowWidth, windowHeight);
+//   canvas = createCanvas(windowWidth, 4.5*windowHeight);
+//   canvas.position(0,0);
 // }
 
+
 function setup() {
-    canvas = createCanvas(windowWidth, 4.2*windowHeight); //make it an object
-    canvas.position(0,0);
+    canvas = createCanvas(windowWidth, 4.5*windowHeight); //make it an object
+    canvas.position(0,0,'fixed');
     canvas.style('z-index','-1');
   noise = new OpenSimplexNoise(Date.now());
   cols = 1 + width / rez;
@@ -53,83 +54,76 @@ function draw() {
   }
   zoff += 0.03;
 
-
-
-
-  //for (let i = 0; i < cols; i++) {
-  //  for (let j = 0; j < rows; j++) {
-  //    fill(field[i][j]*255);
-  //    noStroke();
-  //    rect(i*rez, j*rez, rez, rez);
-  //  }
-  //}
-
-  for (let i = 0; i < cols-1; i++) {
-    for (let j = 0; j < rows-1; j++) {
-      let x = i * rez;
-      let y = j * rez;
-      dx = 0.5;//field[i][j] / (field[i][j]+field[i+1][j]);
-      let a = createVector(x + rez * dx, y            );
-      dx = 0.5;//field[i+1][j+1] / (field[i+1][j]+field[i+1][j+1]);
-      let b = createVector(x + rez, y + rez * dx);
-      dx = 0.5;//field[i][j+1] / (field[i][j+1]+field[i+1][j+1]);
-      let c = createVector(x + rez * dx, y + rez      );
-      dx = 0.5;//field[i][j+1] / (field[i][j]+field[i][j+1]);
-      let d = createVector(x, y + rez * dx);
-      let state = getState(ceil(field[i][j]), ceil(field[i+1][j]), 
-        ceil(field[i+1][j+1]), ceil(field[i][j+1]));
-      stroke(255);
-      strokeWeight(1);
-      switch (state) {
-      case 1:  
-        drawLine(c, d);
-        break;
-      case 2:  
-        drawLine(b, c);
-        break;
-      case 3:  
-        drawLine(b, d);
-        break;
-      case 4:  
-        drawLine(a, b);
-        break;
-      case 5:  
-        drawLine(a, d);
-        drawLine(b, c);
-        break;
-      case 6:  
-        drawLine(a, c);
-        break;
-      case 7:  
-        drawLine(a, d);
-        break;
-      case 8:  
-        drawLine(a, d);
-        break;
-      case 9:  
-        drawLine(a, c);
-        break;
-      case 10: 
-        drawLine(a, b);
-        drawLine(c, d);
-        break;
-      case 11: 
-        drawLine(a, b);
-        break;
-      case 12: 
-        drawLine(b, d);
-        break;
-      case 13: 
-        drawLine(b, c);
-        break;
-      case 14: 
-        drawLine(c, d);
-        break;
+  for (let h = -1; h < 1; h += 1.0) {
+    stroke(255 * h, 255 * -h, 127);
+    strokeWeight(1.5);
+  
+    for (let i = 0; i < cols - 1; i++) {
+      for (let j = 0; j < rows - 1; j++) {
+        let f0 = field[i][j] - h;
+        let f1 = field[i + 1][j] - h;
+        let f2 = field[i + 1][j + 1] - h;
+        let f3 = field[i][j + 1] - h;
+  
+        let x = i * rez;
+        let y = j * rez;
+        let a = createVector(x + rez * f0 / (f0 - f1), y);
+        let b = createVector(x + rez, y + rez * f1 / (f1 - f2));
+        let c = createVector(x + rez * (1 - f2 / (f2 - f3)), y + rez);
+        let d = createVector(x, y + rez * (1 - f3 / (f3 - f0)));
+  
+        let state = getState(f0, f1, f2, f3);
+        switch (state) {
+          case 1:
+            drawLine(c, d);
+            break;
+          case 2:
+            drawLine(b, c);
+            break;
+          case 3:
+            drawLine(b, d);
+            break;
+          case 4:
+            drawLine(a, b);
+            break;
+          case 5:
+            drawLine(a, d);
+            drawLine(b, c);
+            break;
+          case 6:
+            drawLine(a, c);
+            break;
+          case 7:
+            drawLine(a, d);
+            break;
+          case 8:
+            drawLine(a, d);
+            break;
+          case 9:
+            drawLine(a, c);
+            break;
+          case 10:
+            drawLine(a, b);
+            drawLine(c, d);
+            break;
+          case 11:
+            drawLine(a, b);
+            break;
+          case 12:
+            drawLine(b, d);
+            break;
+          case 13:
+            drawLine(b, c);
+            break;
+          case 14:
+            drawLine(c, d);
+            break;
+        }
       }
     }
+    }
   }
-}
-
-function getState(a, b, c, d) {
-  return a * 8 + b * 4  + c * 2 + d * 1;
-}
+  
+  function getState(a, b, c, d) {
+    return (a > 0 ? 8 : 0) + (b > 0 ? 4 : 0) + (c > 0 ? 2 : 0) + (d > 0 ? 1 : 0);
+  }
